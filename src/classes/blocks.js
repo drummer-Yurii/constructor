71bb6eafe0359d501e0091589ac058bde5a367c2
drummer-Yurii <104525685+drummer-Yurii@users.noreply.github.com>
2022-10-26 21:41:34 +0300

    first commit

---
+++ src/classes/blocks.js
@@ -0,0 +1,47 @@
+import {col, css, row} from '../utils'
+
+class Block {
+  constructor(value, options) {
+    this.value = value,
+    this.options = options
+  }
+  toHtml() {
+    throw new Error('Метод toHtml должен быть реализован')
+  }
+}
+
+export class TitleBlock extends Block {
+  constructor(value, options) {
+    super(value, options)
+  }
+  toHtml() {
+    const {tag = 'h1', styles} = this.options
+    return row(col(`<${tag}>${this.value}</${tag}>`), css(styles))
+  }
+}
+export class ImageBlock extends Block {
+  constructor(value, options) {
+    super(value, options)
+  }
+  toHtml() {
+    const {imageStyles: is, alt='', styles} = this.options
+    return row(`<img src="${this.value}" alt="${alt}" style="${css(is)}" />`, css(styles))
+  }
+}
+export class ColumnsBlock extends Block {
+  constructor(value, options) {
+    super(value, options)
+  }
+  toHtml() {
+    const html = this.value.map(col).join('')
+    return row(html, css(this.options.styles))
+  }
+}
+export class TextBlock extends Block {
+  constructor(value, options) {
+    super(value, options)
+  }
+  toHtml() {
+    return row(col(`<p>${this.value}</p>`), css(this.options.styles))
+  }
+}
